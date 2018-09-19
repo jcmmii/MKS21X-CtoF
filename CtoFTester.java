f0329accc6e2d4d72f518d88882614e05292ea55
Justin Chen <jchen06@stuy.edu>
2018-09-19 16:47:21 -0400

    Created functions converting C to F and reverse

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,12 @@
-
+public class CtoFTester {
+  public static double CtoF(double Cels) {
+    return (1.8 * Cels) + 32.0;
+  }
+  public static double FtoC(double Fahr) {
+    return (5.0/9) * (Fahr - 32);
+  }
+  public static void main(String[] args){
+    System.out.println(FtoC(100));
+    System.out.println(CtoF(100));
+}
+}
